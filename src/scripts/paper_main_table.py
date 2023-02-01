4af13d437b6aa07abbe6202c1e3deef5a57cd1f5
Ekaterina Ilin <eilin@aip.de>
2023-02-01 08:26:21 +0100

    edited table header

--- src/scripts/paper_main_table.py
+++ src/scripts/paper_main_table.py
@@ -237,8 +237,8 @@ if __name__ == "__main__":
         "st_lum":"log${10} L_{*}$ [L$_\odot$]",
         "Ro":"Ro",
         "B_G":"$B$ [G]",
-        "p_spi_erg_s":"log$_{10} P_{SPI,sb}$ [erg s$^{-1}$]",
-        "p_spi_kav22":"log$_{10} P_{SPI,aw}$ [erg s$^{-1}$]",
+        "p_spi_erg_s":"log$_{10} P_{sb}$ [erg s$^{-1}$]",
+        "p_spi_kav22":"log$_{10} P_{aw}$ [erg s$^{-1}$]",
         "v_rel_km_s":"$v_{rel}$ [km s$^{-1}$]",
         "mean":"p-value",
         }
@@ -298,26 +298,6 @@ if __name__ == "__main__":
     del singles["multiple_star_source"]
     del singles["orbits_covered"]
 
-    # make TeX table
-    # print("Converting table to TeX format.")
-
-    # # make the default string length longer
-    # pd.set_option('display.max_colwidth', 1000)
-
-    # string = singles.to_latex(escape=False,index=False)
-
-    # # layout
-    # string = string.replace("\citet{[*]}","[*]")
-    # string = string.replace("midrule","hline")
-    # string = string.replace("toprule","hline")
-    # string = string.replace("bottomrule","hline")
-
-    # # write to file
-    # path = paths.output / "fit_parameters.tex"
-    # print("Write to file: ", path)
-
-    # with open(path, "w") as f:
-    #     f.write(string)
 
 
     # Get all bibkeys from singles table
@@ -334,26 +314,13 @@ if __name__ == "__main__":
     # make a dictionary with the bibkeys and the corresponding index
     bibkeys = {bibkeys[i]:i+1 for i in range(len(bibkeys))}
 
-    # # replace the bibkeys with the index in the string table
-    # for key in bibkeys.keys():
-    #     replace = "\citet{"+key+"}"
-    #     string = string.replace(replace, "(" + str(bibkeys[key]) + ")")
-
-    # # write to file
-    # path = paths.output / "fit_parameters_bibkeys.tex"
-    # print("Write to file: ", path)
-
-    # with open(path, "w") as f:
-    #     f.write(string)
-
-
     # literature parameters table columns
-    lit_cols = ["ID", "TIC", "$P_{rot}$ [d]", "$P_{orb}$ [d]", "$R_{*}$ [R$_\odot$]",
+    lit_cols = ["ID", "$P_{rot}$ [d]", "$P_{orb}$ [d]", "$R_{*}$ [R$_\odot$]",
             "$R_{p}$ [R$_J$]", "$a$ [au]", "log${10} L_{*}$ [L$_\odot$]"]
 
     # derived parameters table columns
-    der_cols = ["ID", "Ro", "$B$ [G]", "log$_{10} P_{SPI,sb}$ [erg s$^{-1}$]",
-                "log$_{10} P_{SPI,aw}$ [erg s$^{-1}$]", "$v_{rel}$ [km s$^{-1}$]",
+    der_cols = ["ID", "Ro", "$B$ [G]", "log$_{10} P_{sb}$ [erg s$^{-1}$]",
+                "log$_{10} P_{aw}$ [erg s$^{-1}$]", "$v_{rel}$ [km s$^{-1}$]",
                 "p-value",]
 
     # make a list of tuples with the two column lists
