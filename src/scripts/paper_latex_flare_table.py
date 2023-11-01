4f1404367e4aa79363242f3069f10667f2d2333d
Ekaterina Ilin <eilin@aip.de>
2023-11-01 15:43:09 +0100

    try again

--- src/scripts/paper_latex_flare_table.py
+++ src/scripts/paper_latex_flare_table.py
@@ -27,8 +27,6 @@ if __name__ == "__main__":
     flare_table["rel_amplitude"] = flare_table.apply(lambda x: fr"${x.rel_amplitude:.4f}$",
                                                      axis=1)
 
-
-
     # Get 4 digits after decimal point for tstart column
     flare_table["tstart"] = flare_table.apply(lambda x: fr"${x.tstart:.4f}$",
                                               axis=1)
